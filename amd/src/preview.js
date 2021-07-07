a77355495879757ee03f0c1f550a5ee890994e78
Daniel Thies <dethies@gmail.com>
2021-07-06 23:51:07 -0500

    Style fix

--- amd/src/preview.js
+++ amd/src/preview.js
@@ -11,13 +11,13 @@ const previewQuestion = function(e) {
         let url = anchor.href,
             height = Math.min(600, window.screen.height),
             width = Math.min(600, window.screen.width),
-            xOffset = (window.screen.width - width)/2,
-            yOffset = (window.screen.height - height)/2;
+            xOffset = (window.screen.width - width) / 2,
+            yOffset = (window.screen.height - height) / 2;
         e.stopPropagation();
         e.preventDefault();
         window.open(url, 'repurpose_preview', 'width=' + width + ',height=' + height
-            + ',left=' + xOffset +',top=' + yOffset
-            + ',xOffset=' + xOffset +',yOffset=' + yOffset
+            + ',left=' + xOffset + ',top=' + yOffset
+            + ',xOffset=' + xOffset + ',yOffset=' + yOffset
         );
     }
 };
