82c1cd23cdf0030778e48755ce8d0366b7d0bb33
Germán Valero <gvalero@unam.mx>
2021-07-08 16:34:44 -0500

    Add Spanish language strings

---
+++ lang/es_mx/contenttype_repurpose.php
@@ -0,0 +1,58 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     contenttype_repurpose
+ * @category    string
+ * @copyright   2020 onward Daniel Thies <dethies@gmail.com>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Reciclando recursos';
+$string['description'] = 'Reciclando recursos';
+$string['duration'] = 'Duración';
+$string['example'] = 'Ejemplo';
+$string['gotoquestionbank'] = 'Ir al Banco de preguntas';
+$string['repurpose:access'] = 'Acceder a recursos reciclados en el Banco de contenido';
+$string['repurpose:upload'] = 'Subir recursos para usar en contenido';
+$string['repurpose:useeditor'] = 'Crear contenido usando las herramientas para reciclar recursos';
+$string['importdialogcards'] = 'Importar Tarjetas de Diálogo';
+$string['importcolumn'] = 'Importar a Columna';
+$string['importcoursepresentation'] = 'Importar a Presentación de Curso';
+$string['importflashcards'] = 'Importar Tarjetas de Memoria';
+$string['importsinglechoiceset'] = 'Importar onjunto de Opción Única';
+$string['importquestionset'] = 'Importar un Conjunto de Pregunta';
+$string['importquestion'] = 'Importar una sola pregunta';
+$string['importvideo'] = 'Importar a un Video Interactivo';
+$string['license'] = 'Licencia';
+$string['license_help'] = 'Elegir la licencia a especificar para el tipo de contenido';
+$string['name'] = 'Nombre';
+$string['name_help'] = 'Escribir nombre para el contenido o dejar vacío para predeterminado de pregunta o categoría';
+$string['noquestionsselected'] = 'No hay preguntas disponibles en la selección actual';
+$string['previewquestion'] = 'Previsualizar pregunta';
+$string['previewquestion_help'] = 'Un enlace es proporcionado para previsualizar la pregunta seleccionada si usted tiene permiso para hacerlo. Hacer clic en el enlace debería abrir unanueva ventana';
+$string['privacy:metadata'] = 'El plugin para Reciclar contenido no almacena ninguna información personal';
+$string['slideno'] = 'Página {$a}';
+$string['starttime'] = 'Hora de inicio';
+$string['tutorial'] = 'Tutorial';
+$string['question'] = 'pregunta';
+$string['question_help'] = 'Si usted ha creado o importado preguntas
+en el Banco de contenido, uste debería de poder seleccionarlas individualmente y
+convertirlas a H5P';
