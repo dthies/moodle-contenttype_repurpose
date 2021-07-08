c6a396eb5ef6c65314e54016e6ebfcc2e90de7a4
Daniel Thies <dethies@gmail.com>
2021-07-08 18:31:29 -0500

    Update version.php

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'contenttype_repurpose';
 $plugin->release = '1.0';
-$plugin->version = 2021070401;
+$plugin->version = 2021070402;
 $plugin->requires = 2020061502.06; // Requires this Moodle version 3.9.2.
 $plugin->maturity = MATURITY_BETA;
