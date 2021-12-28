cda7a8a8ecc7e663a9d945a6d396c5f259b9de78
Daniel Thies <dethies@gmail.com>
2021-12-28 11:03:08 -0600

    Clean up js

--- amd/src/formupdate.js
+++ amd/src/formupdate.js
@@ -2,6 +2,12 @@ import Fragment from 'core/fragment';
 import notification from 'core/notification';
 import templates from 'core/templates';
 
+/**
+ * Initialize listeners
+ *
+ * @param {int} contextid Context id of content bank
+ * @param {string} library Library identifier
+ */
 export const init = (contextid, library) => {
     'use strict';
 
@@ -9,26 +15,36 @@ export const init = (contextid, library) => {
     document.addEventListener('change', updateForm.bind(window, contextid, library));
 };
 
+/**
+ * Update the form
+ *
+ * @param {int} contextid Context id of content bank
+ * @param {string} library Library identifier
+ * @param {event} e Event
+ */
 const updateForm = function(contextid, library, e) {
-    if (e.target.closest('#id_category, #id_recurse, #id_question, [data-action="update"]')) {
+    if (e.target.closest('form #id_category, form #id_recurse, form #id_question, form [data-action="update"]')) {
+        let data = {},
+            form = e.target.closest('form'),
+            formdata = new FormData(form),
+            params = {
+                contextid: contextid,
+                jsonformdata: JSON.stringify(data),
+                library: library,
+                plugin: 'repurpose'
+            };
+
         e.stopPropagation();
+        e.preventDefault();
         window.onbeforeunload = null;
-        let form = e.target;
-        while (form.nodeName.toLowerCase() !== 'form') {
-            form = form.parentNode;
-        }
-        let formdata = new FormData(form),
-            data = {};
+
         formdata.forEach((value, key) => {
             data[key] = value;
         });
-        let params = {
-            contextid: contextid,
-            jsonformdata: JSON.stringify(data),
-            library: library,
-            plugin: 'repurpose'
-        };
+        params.jsonformdata = JSON.stringify(data);
+
         Fragment.loadFragment('contenttype_repurpose', 'formupdate', contextid, params).done(function(html, js) {
+            document.removeEventListener('change', updateForm.bind(window, contextid, library));
             templates.replaceNodeContents(form, html, js);
             form.parentNode.insertBefore(form.firstChild, form);
             form.remove();
